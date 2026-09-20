5253a3b76b6792a79fe5fb575715460beb33cc48
jcrespoext_utpGH <jcrespoext@utp.edu.pe>
2026-09-20 17:12:40 -0500

    feat: Add CD with aegocd cli v6

--- src/app.py
+++ src/app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 def details():
     return  jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"),
-        'hostname': socket.gethostname(),
-        'message': 'You are doing great humans...!!'
+        'message': 'You are doing great animals...!!',
+        'hostname': socket.gethostname()
     })
 
 @app.route('/api/v1/healthz')
