f76462105648f98d42810fab311608aee0b2ff43
jcrespoext_utpGH <jcrespoext@utp.edu.pe>
2026-09-20 19:57:49 -0500

    feat: modify values file on CD step v6

--- src/app.py
+++ src/app.py
@@ -4,13 +4,14 @@ import socket
 
 app = Flask(__name__)
 
-@app.route('/api/v1/details')
+@app.route('/api/v1/info')
 
-def details():
+def info():
     return  jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"),
         'message': 'You are doing great humans and plants)!',
-        'hostname': socket.gethostname()
+        'hostname': socket.gethostname(),
+        'deploy_on': 'kubernetes' 
     })
 
 @app.route('/api/v1/healthz')
